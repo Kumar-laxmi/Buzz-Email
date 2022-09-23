53e6f091f13da5d2e0a5baed34116479c67f62ca
Kumar-laxmi <laxmikantk2002@gmail.com>
2022-09-23 14:30:27 +0530

    Mail service integration PART-1

--- app/views.py
+++ app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 def index(request):
+    return render(request, 'index.html', {})
+
+def index2(request, emails, message):
+    send_mail(
+        'This is a bot generated email',
+        message,
+        'noreply@buzzemail.com',
+        emails,
+        fail_silently=False,
+    )
     return render(request, 'index.html', {})
--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ from app.views import *
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', index),
+    path('send/<str:emails>/<str:message>', index2),
 ]
